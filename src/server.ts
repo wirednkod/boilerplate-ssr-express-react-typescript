df3eb896df798c888468ba8428244737c3a66b08
Nikolaos Kontakis <wirednkod@gmail.com>
2020-03-09 16:01:03 +0200

    remove webpack add nodemon

--- src/server.ts
+++ src/server.ts
@@ -23,24 +23,3 @@ const app = new App({
 })
 
 app.listen()
-
-type ModuleId = string | number;
-
-interface WebpackHotModule {
-    hot?: {
-        data: any;
-        accept(
-            dependencies: string[],
-            callback?: (updatedDependencies: ModuleId[]) => void,
-        ): void;
-        accept(dependency: string, callback?: () => void): void;
-        accept(errHandler?: (err: Error) => void): void;
-        dispose(callback: (data: any) => void): void;
-    };
-}
-
-declare const module: WebpackHotModule
-
-if (module.hot) {
-    module.hot.accept()
-}
